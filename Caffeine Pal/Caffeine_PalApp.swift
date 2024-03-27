997f22f640afaa86d53a99f0ae80bb7e9d18246e
Fluffik3666 <110257462+Fluffik3666@users.noreply.github.com>
2024-03-27 11:05:38 +0000

    Initial Commit

---
+++ Caffeine Pal/Caffeine_PalApp.swift
@@ -0,0 +1,32 @@
+//
+//  Caffeine_PalApp.swift
+//  Caffeine Pal
+//
+//  Created by Sasha Bagrov on 27/03/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Caffeine_PalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
